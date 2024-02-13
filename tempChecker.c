73a32d47a7a884dc088503fab145f3683fc3a2b0
Sebagabones <bones@ucc.asn.au>
2024-02-13 21:43:50 +0800

    Fan speed updatesss

--- tempChecker.c
+++ tempChecker.c
@@ -5,13 +5,11 @@
 #include <sys/sysinfo.h>
 #include <stdbool.h>
 #include <getopt.h>
-#include "fanTester.h"
-#define	OPTLIST		"dvhts:"
+
+#define	OPTLIST		"vh"
 
 bool verbose = false;
-bool dryRun = false;
-bool fanTest = false;
-int seconds = 5;
+
 int temps(int numCores)
 {
 	FILE *fp;
@@ -54,20 +52,17 @@ int temps(int numCores)
 			check = 1;
 		}
 	}
-	if(!dryRun)
+	if(check == 2)
 	{
-		if(check == 2)
-		{
-			system("sudo ipmitool raw 0x3a 0x07 0x01 0xff 0x01 && sudo ipmitool raw 0x3a 0x07 0x02 0xff 0x01");
-		}
-		else if(check == 1)
-		{
-			system("sudo ipmitool raw 0x3a 0x07 0x01 0xff 0x00 &&  sudo ipmitool raw 0x3a 0x07 0x02 0xff 0x00");
-		}
-		else
-		{
-			system("sudo ipmitool raw 0x3a 0x07 0x02 0x27 0x01 && sudo ipmitool raw 0x3a 0x07 0x01 0x27 0x01");
-		}
+		system("sudo ipmitool raw 0x3a 0x07 0x01 0xff 0x01 && sudo ipmitool raw 0x3a 0x07 0x02 0xff 0x01");
+	}
+	else if(check == 1)
+	{
+		system("sudo ipmitool raw 0x3a 0x07 0x01 0xff 0x00 &&  sudo ipmitool raw 0x3a 0x07 0x02 0xff 0x00");
+	}
+	else
+	{
+		system("sudo ipmitool raw 0x3a 0x07 0x02 0x34 0x01 && sudo ipmitool raw 0x3a 0x07 0x01 0x34 0x01");
 	}
 	pclose(fp);
 	return check;
@@ -76,103 +71,9 @@ int temps(int numCores)
 
 int main(int argc, char * argv[])
 {
-    int  opt;
-    opterr = 0;
-    while((opt = getopt(argc, argv, OPTLIST)) != -1)   
-    {
-//  verbose flag option
-        if(opt == 'v')
-        {
-                verbose = true;
-                printf("verbose output\n");
-        }
-		else if(opt == 'h')
-		{
-			printf("Run with -v for verbose, or -d for a dry run, t for fan noise testing, and if you want the fans to run longer when testing us -s (INT) for number of seconds to run for\n");
-			exit(0);
-		}
-		else if(opt == 'd')
-		{
-			dryRun = true;
-			verbose = true;
-		}
-		else if(opt == 't')
-		{
-			printf("Testing fan speeds noise levels\n");
-			fanTest = true;
-		}
-		else if(opt == 's')
-		{
-			seconds = atoi(optarg);
-		}
-	}
-	int fan = 0;
-	char cont;
-	bool loopVal = true;
-	if(fanTest)
-	{
-		while(loopVal)
-		{
-		    char fanspeed[2];
-			char command[100];
-		    printf("\n\n\033[31;1;4mTurning off fanServ.service - (this should turn back on automatically, but if you ctrl + C make sure to turn this back on)\033[0m\n\n");
-			system("sudo systemctl stop fanServ.service");
-		    printf("Please enter fan speed (between 00 and ff) you want to test noise levels of (hexadecimal):\n");
-		    scanf("%s",&fanspeed);
-		    printf("fanspeed chosen is %s, testing: \n", fanspeed);
-	        snprintf(command, sizeof(command), "sudo ipmitool raw 0x3a 0x07 0x01 0x%s 0x01 && sudo ipmitool raw 0x3a 0x07 0x02 0x%s 0x01",fanspeed, fanspeed);
-			printf("%s\n", command);
-			printf("\033[31mrunning:\033[0m\n");
-	        system(command);
-			sleep(seconds);
-			printf("\033[32mfinished running\n\033[0m");
-		    // test fan speed
-		    system("sudo systemctl start fanServ.service");
- 			printf("\n\033[31;1;4mRestoring to default fan settings - turning fanServ.service back on\033[0m\n");
-		    printf("Would you like to test another speed(y) or exit(any other key)\n");
-		    scanf(" %c", &cont);
-			if(cont != 'y')
-			{
-				loopVal = false;
-			}
-		}
-	}
-//	else
-//	{
-//		printf("fan %i\n", fan);
-		int numberOfCores = (get_nprocs() / 2);
-		if(verbose)
-		{
-			printf("number of cores is %i\n", numberOfCores);
-		}
-		int temp = temps(numberOfCores);
-		if(verbose)
-		{
-			if(dryRun)
-			{
-				printf("Dry run, although I would have run: ");
-			}
-			else
-			{
-				printf("Ran ");
-			}
-			if(temp == 0)
-			{
-				printf("'sudo ipmitool raw 0x3a 0x07 0x02 0x27 0x01 && sudo ipmitool raw 0x3a 0x07 0x01 0x27 0x01'\n");
-				printf("(lowest fan speed)\n");
-			}
-			else if(temp == 1)
-			{
-				printf("'sudo ipmitool raw 0x3a 0x07 0x01 0xff 0x00 &&  sudo ipmitool raw 0x3a 0x07 0x02 0xff 0x00'\n");
-				printf("(medium fan speed)\n");
-			}
-			else if(temp == 0)
-			{
-				printf("'sudo ipmitool raw 0x3a 0x07 0x01 0xff 0x01 && sudo ipmitool raw 0x3a 0x07 0x02 0xff 0x01'\n");
-				printf("(highest fan speed)\n");
-			}
-		}
-		return 0;
-//	}
+//	int numberOfCores = (atoi(argv[1]));
+	int numberOfCores = (get_nprocs() / 2);
+	int temp = temps(numberOfCores);
+
 	return 0;
 }
